850f1f8ec5bf192ff474bae315a9d6877eae9983
Cat Janowitz and Grant Rotzien <grant@>
2017-10-23 11:09:06 -0700

    successfully isolate elements from original array and rearrange in new array and display as a ul

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,15 @@ $(document).ready(function() {
     var b = $("input#fav2").val();
     var c = $("input#fav3").val();
     favThingsArray = [a, b, c];
-    console.log(favThingsArray);
     console.log(favThingsArray.toString());
+    var newArray = [];
+      newArray.push(favThingsArray[1],favThingsArray[0],favThingsArray[2]);
+      console.log(newArray);
+
+  $("li#1").text(newArray[0]);
+  $("li#2").text(newArray[1]);
+  $("li#3").text(newArray[2]);
+
+
   });
 });
